5827cc62f5ad02c7757405651d06cf3a50f0b1b7
Ryan Kowalick <rkowalick@covermymeds.com>
2015-02-09 21:22:08 -0500

    Simplify code using pack, unpack

--- conversion.rb
+++ conversion.rb
@@ -1,32 +1,23 @@
 require 'minitest/autorun'
+require 'pry'
 
 LOOKUP = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 + /).freeze
 
 def hex_string_to_base_64(hex_string)
-  binary = hex_string_to_binary_string(hex_string)
-
-  binary = binary + "0" * (6 - binary.size % 6) unless binary.size % 6 == 0
-
-  accum = ""
-  binary.each_char.each_slice(6) do |s|
-    accum << LOOKUP[Integer(s.join(''), 2)]
-  end
-  accum
-end
-
-def hex_string_to_binary_string(hex_string)
-  binary = ""
-  hex_string.each_char.each do |c|
-    binary << "%04b" % Integer(c, 16)
-  end
-  binary
+  binding.pry
+  string_to_base_64(hex_string_to_string(hex_string))
 end
 
 def hex_string_to_string(hex_string)
-  chars = hex_string.each_char.each_slice(2).map do |hex1, hex2|
-    Integer(hex1 + hex2, 16)
-  end
-  chars.map(&:chr).join
+  [hex_string].pack("H*")
+end
+
+def string_to_hex_string(string)
+  string.unpack("H*").first
+end
+
+def string_to_binary_string(string)
+  string.unpack("B*").first
 end
 
 def hex_string_to_bytes(hex_string)
@@ -44,7 +35,7 @@ def base_64_to_string(input)
 end
 
 def string_to_base_64(input)
-  [input].pack("m")
+  [input].pack("m0")
 end
 
 def bytes_to_hex(bytes)
@@ -66,12 +57,12 @@ Class.new(Minitest::Test) do
 
   def test_base_64_to_string
     output = "any carnal pleasur"
-    input = "YW55IGNhcm5hbCBwbGVhc3Vy\n"
+    input = "YW55IGNhcm5hbCBwbGVhc3Vy"
     assert_equal(output, base_64_to_string(input))
   end
 
   def test_string_to_base_64
-    output = "YW55IGNhcm5hbCBwbGVhc3Vy\n"
+    output = "YW55IGNhcm5hbCBwbGVhc3Vy"
     input = "any carnal pleasur"
     assert_equal(output, string_to_base_64(input))
   end
